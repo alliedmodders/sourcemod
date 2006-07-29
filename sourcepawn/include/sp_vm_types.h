8f0e90ee294981fd67e0fa3a30513c93c78f9b89
David Anderson <dvander@alliedmods.net>
2006-07-29 21:02:28 +0000

    initial import of API
    --HG--
    extra : convert_revision : svn%3A39bc706e-5318-0410-9160-8a85361fbb7c/trunk%4038

---
+++ sourcepawn/include/sp_vm_types.h
@@ -0,0 +1,202 @@
+#ifndef _INCLUDE_SOURCEPAWN_VM_TYPES_H
+#define _INCLUDE_SOURCEPAWN_VM_TYPES_H
+
+#include "sp_file_headers.h"
+
+typedef	uint32_t	ucell_t;
+typedef int32_t		cell_t;
+
+/**
+ * Error codes
+ */
+#define SP_ERR_NONE					0
+#define SP_ERR_FILE_FORMAT			1	/* File format unrecognized */
+#define SP_ERR_DECOMPRESSOR			2	/* A decompressor was not found */
+
+/**********************************************
+ *** The following structures are reference structures.
+ *** They are not essential to the API, but are used
+ ***  to hold the backend database format of the plugin
+ ***  binary.
+ **********************************************/
+
+/**
+ * Information about the core plugin tables.
+ * These may or may not be present!
+ */
+typedef struct sp_plugin_infotab_t
+{
+	const char *stringbase;		/* base of string table */
+	uint32_t	publics_num;	/* number of publics */
+	sp_file_publics_t *publics;	/* public table */
+	uint32_t	natives_num;	/* number of natives */
+	sp_file_natives_t *natives; /* native table */
+	uint32_t	pubvars_num;	/* number of pubvars */
+	sp_file_pubvars_t *pubvars;	/* pubvars table */	
+} sp_plugin_infotab_s;
+
+/**
+ * Information about the plugin's debug tables.
+ * These are all present if one is present.
+ */
+typedef struct sp_plugin_debug_t
+{
+	const char *stringbase;		/* base of string table */
+	uint32_t	files_num;		/* number of files */
+	sp_fdbg_file_t *files;		/* files table */
+	uint32_t	lines_num;		/* number of lines */
+	sp_fdbg_line_t *lines;		/* lines table */
+	uint32_t	syms_num;		/* number of symbols */
+	sp_fdbg_symbol_t *symbols;	/* symbol table */
+} sp_plugin_debug_s;
+
+#define SP_FA_SELF_EXTERNAL		(1<<0)
+#define SP_FA_BASE_EXTERNAL		(1<<1)
+
+/**
+ * The rebased, in-memory format of a plugin.
+ * This differs from the on-disk structure to ensure
+ *  that the format is properly read.
+ */
+typedef struct sp_plugin_t
+{
+	uint8_t		*base;			/* base of memory */
+	uint8_t		*pcode;			/* p-code */
+	uint32_t	pcode_size;		/* size of p-code */
+	uint8_t		*data;			/* data size */
+	uint32_t	data_size;		/* size of data */
+	uint32_t	memory;			/* required memory */
+	uint16_t	flags;			/* code flags */
+	uint32_t	allocflags;		/* allocation flags */
+	sp_plugin_infotab_t *info;	/* base info table */
+	sp_plugin_debug_t *debug;	/* debug info table */
+} sp_plugin_s;
+
+struct sp_context_s;
+typedef int (*SPVM_NATIVE_FUNC)(sp_context_s *, cell_t *);
+
+/**********************************************
+ *** The following structures are bound to the VM/JIT.
+ *** Changing them will result in necessary recompilation.
+ **********************************************/
+
+/**
+ * Offsets and names to a public function.
+ * By default, these point back to the string table 
+ *  in the sp_plugin_infotab_t structure.
+ */
+typedef struct sp_public_t
+{
+	uint32_t	offs;		/* code offset */
+	const char *name;		/* name */
+} sp_publics_s;
+
+/**
+ * Offsets and names to public variables.
+ * The offset is relocated and the name by default
+ *  points back to the sp_plugin_infotab_t structure.
+ */
+typedef struct sp_pubvar_t
+{
+	cell_t	   *offs;		/* pointer to data */
+	const char *name;		/* name */
+} sp_pubvar_t 
+
+#define SP_NATIVE_NONE		(0)		/* Native is not yet found */
+#define SP_NATIVE_OKAY		(1)		/* Native has been added */
+#define SP_NATIVE_PENDING	(2)		/* Native is marked as usable but replaceable */
+
+/** 
+ * Native lookup table, by default names
+ *  point back to the sp_plugin_infotab_t structure.
+ * A native is NULL if unit
+ */
+typedef struct sp_native_t
+{
+	SPVM_NATIVE_FUNC	pfn;	/* function pointer */
+	const char *		name;	/* name of function */
+	uint32_t			status;	/* status flags */
+} sp_native_t;
+
+/** 
+ * Debug file table
+ */
+typedef struct sp_debug_file_s
+{
+	uint32_t		addr;	/* address into code */
+	const char *	name;	/* name of file */
+} sp_debug_file_t;
+
+/**
+ * Note that line is missing.  It is not necessary since
+ *  this can be retrieved from the base plugin info.
+ */
+typedef struct sp_debug_line_s
+{
+	uint32_t		addr;	/* address into code */
+	uint32_t		line;	/* line no. */
+} sp_debug_line_t;
+
+typedef sp_fdbg_arraydim_t	sp_debug_arraydim_t;
+
+/**
+ * The majority of this struct is already located in the parent 
+ *  block.  Thus, only the relocated portions are required.
+ */
+typedef struct sp_debug_symbol_s
+{
+	uint32_t		codestart;	/* relocated code address */
+	uint32_t		codeend;	/* relocated code end address */
+	const char *	name;		/* relocated name */
+	sp_debug_arraydim_t *dims;	/* relocated dimension struct, if any */
+	sp_fdbg_symbol_t	*sym;	/* pointer to original symbol */
+} sp_debug_symbol_t;
+
+/**
+ * Executes a Context.
+ * @sp_context_s	- Execution Context
+ * @uint32_t		- Offset from code pointer
+ * @res				- return value of function
+ * @return			- error code (0=none)
+ */
+typedef int (*SPVM_EXEC)(struct sp_context_s *,
+							uint32_t,
+							cell_t *res);
+
+#define SP_CONTEXT_DEBUG			(1<<0)		/* in debug mode */
+#define SP_CONTEXT_INHERIT_MEMORY	(1<<1)		/* inherits memory pointers */
+#define SP_CONTEXT_INHERIT_CODE		(1<<2)		/* inherits code pointers */
+
+/**
+ * This is the heart of the VM.  It contains all of the runtime
+ *  information about a plugin context.  
+ * It is split into three sections.
+ */
+typedef struct sp_context_s
+{
+	/* parent information */
+	void			*base;		/* base of generated code and memory */
+	sp_plugin_t		*plugin;	/* pointer back to parent information */
+	struct sp_context_s *parent;	/* pointer to parent context */
+	uint32_t		flags;		/* context flags */
+	/* execution specific data */
+	SPVM_EXEC		exec;		/* execution base */
+	cell_t			pri;		/* PRI register */
+	cell_t			alt;		/* ALT register */
+	cell_t			*data;		/* data chunk */
+	cell_t			*heap;		/* pointer after data for start of heap */
+	cell_t			*sp;		/* stack pointer */
+	ucell_t			memory;		/* total memory size; */
+	int32_t			err;		/* error code */
+	uint32_t		pushcount;	/* push count */
+	/* context rebased database */
+	sp_public_t		*publics	/* public functions table */
+	sp_pubvar_t		*pubvars;	/* public variables table */
+	sp_native_t		*natives;	/* natives table */
+	sp_debug_file_t	*files;		/* files */
+	sp_debug_line_t	*lines;		/* lines */
+	sp_debug_symbol_t *symbols;	/* symbols */
+} sp_context_t;
+								
+#endif //_INCLUDE_SOURCEPAWN_VM_TYPES_H
+
---
+++ sourcepawn/vm/sp_vm.h
@@ -0,0 +1,273 @@
+#ifndef _INCLUDE_SOURCEPAWN_VM_H_
+#define _INCLUDE_SOURCEPAWN_VM_H_
+
+#include "sp_vm_types.h"
+
+/*****************
+ ** Note that all functions return a non-zero error code on failure
+ *   unless otherwise noted.
+ * All input pointers must be valid unless otherwise noted as optional.
+ * All local address are guaranteed to be positive.  However, they are stored
+ *  as signed integers, because they must logically fit inside a cell.
+ */
+
+typedef struct sp_nativeinfo_s
+{
+	const char		*name;
+	SPVM_NATIVE_FUNC func;
+} sp_nativeinfo_t;
+
+/**
+ * Loads a named file from a file pointer.  
+ * Using this means base memory will be allocated by the VM.
+ * 
+ * @param fp		File pointer.  May be at any offset.  Not closed on return.
+ * @param err		Optional error code pointer.
+ * @return			A new plugin structure.
+ */
+sp_plugin_t *SP_LoadFromFilePointer(FILE *fp, int *err);
+
+/**
+ * Loads a file from a base memory address.
+ *
+ * @param base		Base address of the plugin's memory region.
+ * @param plugin	If NULL, a new plugin pointer is returned.
+ *                  Otherwise, the passed pointer is used.
+ * @param err		Optional error code pointer.
+ * @return			The resulting plugin pointer.
+ */
+sp_plugin_t *SP_LoadFromMemory(void *base, sp_plugin_t *plugin, int *err);
+
+/**
+ * Frees all of the memory associated with a plugin file.
+ * If allocated using SP_LoadFromMemory, the base and plugin pointer 
+ *  itself are not freed (so this may end up doing nothing).
+ */
+int SP_FreeFromMemory(sp_plugin_t *plugin);
+
+/**
+ * Allocs memory on the secondary stack of a plugin.
+ * Note that although called a heap, it is in fact a stack.
+ * 
+ * @param ctx			Context pointer.
+ * @param cells			Number of cells to allocate.
+ * @param local_adddr	Will be filled with data offset to heap.
+ * @param phys_addr		Physical address to heap memory.
+ */
+int SP_HeapAlloc(sp_context_t *ctx, unsigned int cells, cell_t *local_addr, cell_t **phys_addr);
+
+/**
+ * Pops a heap address off the heap stack.  Use this to free memory allocated with
+ *  SP_HeapAlloc().  
+ * Note that in SourcePawn, the heap is in fact a bottom-up stack.  Deallocations
+ *  with this native should be performed in precisely the REVERSE order.
+ */
+int SP_HeapPop(sp_context_t *ctx, cell_t local_addr);
+
+/**
+ * Releases a heap address using a different method than SP_HeapPop().
+ * This allows you to release in any order.  However, if you allocate N 
+ *  objects, release only some of them, then begin allocating again, 
+ *  you cannot go back and starting freeing the originals.  
+ * In other words, for each chain of allocations, if you start deallocating,
+ *  then allocating more in a chain, you must only deallocate from the current 
+ *  allocation chain.  This is basically SP_HeapPop() except on a larger scale.
+ */
+int SP_HeapRelease(sp_context_t *ctx, cell_t local_addr);
+
+/**
+ * Finds a native by name.
+ *
+ * @param ctx			Context pointer.
+ * @param name			Name of native.
+ * @param index			Optionally filled with native index number.
+ */
+int SP_FindNativeByName(sp_context_t *ctx, const char *name, uint32_t *index);
+
+/**
+ * Gets native info by index.
+ *
+ * @param ctx			Context pointer.
+ * @param index			Index number of native.
+ * @param native		Optionally filled with pointer to native structure.
+ */
+int SP_GetNativeByIndex(sp_context_t *ctx, uint32_t index, sp_native_t **native);
+
+/**
+ * Gets the number of natives.
+ * 
+ * @param ctx			Context pointer.
+ * @param num			Filled with the number of natives.
+ */
+int SP_GetNativesNum(sp_context_t *ctx, uint32_t *num);
+
+/**
+ * Finds a public function by name.
+ *
+ * @param ctx			Context pointer.
+ * @param name			Name of public
+ * @param index			Optionally filled with public index number.
+ */
+int SP_FindPublicByName(sp_context_t *ctx, const char *name, uint32_t *index);
+
+
+/**
+ * Gets public function info by index.
+ * 
+ * @param ctx			Context pointer.
+ * @param index			Public function index number.
+ * @param pblic			Optionally filled with pointer to public structure.
+ */
+int SP_GetPublicByIndex(sp_context_t *ctx, uint32_t index, sp_public_t **pblic);
+
+/**
+ * Gets the number of public functions.
+ *
+ * @param ctx			Context pointer.
+ * @param num			Filled with the number of public functions.
+ */
+int SP_GetPublicsNum(sp_context_t *ctx, uint32_t *num);
+
+/**
+ * Gets public variable info by index.
+ * @param ctx			Context pointer.
+ * @param index			Public variable index number.
+ * @param pubvar		Optionally filled with pointer to pubvar structure.
+ */
+int SP_GetPubvarByIndex(sp_context_t *ctx, uint32_t index, sp_pubvar_t **pubvar);
+
+/**
+ * Finds a public variable by name.
+ *
+ * @param ctx			Context pointer.
+ * @param name			Name of pubvar
+ * @param index			Optionally filled with pubvar index number.
+ * @param local_addr	Optionally filled with local address offset.
+ * @param phys_addr		Optionally filled with relocated physical address.
+ */
+int SP_FindPubvarByName(sp_context_t *ctx, 
+						const char *name, 
+						uint32_t *index, 
+						cell_t *local_addr, 
+						cell_t **phys_addr);
+
+/**
+ * Round-about method of converting a plugin reference to a physical address
+ *
+ * @param ctx			Context pointer.
+ * @param local_addr	Local address in plugin.
+ * @param phys_addr		Optionally filled with relocated physical address.
+ */
+int SP_LocalToPhysAddr(sp_context_t *ctx, cell_t local_addr, cell_t **phys_addr);
+
+/**
+ * Convers a local address to a physical string.
+ * Note that SourcePawn does not support packed strings, as such this function is
+ *  'cell to char' only.
+ *
+ * @param ctx			Context pointer.
+ * @param local_addr	Local address in plugin.
+ * @param chars			Optionally filled with the number of characters written.
+ * @param buffer		Destination output buffer.
+ * @param maxlength		Maximum length of output buffer, including null terminator.
+ */
+int SP_LocalToString(sp_context_t *ctx, 
+					cell_t local_addr, 
+					int *chars, 
+					char *buffer, 
+					size_t maxlength);
+
+/**
+ * Pushes a cell onto the stack.  Increases the parameter count by one.
+ *
+ * @param ctx			Context pointer.
+ * @param value			Cell value.
+ */
+int SP_PushCell(sp_context_t *ctx, cell_t value);
+
+/** 
+ * Pushes an array of cells onto the stack.  Increases the parameter count by one.
+ * Note that this does not release the heap, so you should release it after
+ *  calling SP_Execute().
+ *
+ * @param ctx			Context pointer.
+ * @param local_addr	Filled with local address to release.
+ * @param phys_addr		Optionally filled with physical address of new array.
+ * @param array			Cell array to copy.
+ * @param numcells		Number of cells in the array to copy.
+ */
+int SP_PushCellArray(sp_context_t *ctx, 
+					 cell_t *local_addr,
+					 cell_t **phys_addr,
+					 cell_t array[],
+					 unsigned int numcells);
+
+/**
+ * Pushes a string onto the stack (by reference) and increases the parameter count by one.
+ * Note that this does not release the heap, so you should release it after
+ *  calling SP_Execute().
+ *
+ * @param ctx			Context pointer.
+ * @param local_addr	Filled with local address to release.
+ * @param phys_addr		Optionally filled with physical address of new array.
+ * @param array			Cell array to copy.
+ * @param numcells		Number of cells in the array to copy.
+ */
+int SP_PushString(sp_context_t *ctx,
+				  cell_t *local_addr,
+				  cell_t **phys_addr,
+				  const char *string);
+
+/**
+ * Individually pushes each cell of an array of cells onto the stack.  Increases the 
+ *  parameter count by the number of cells pushed.
+ *
+ * @param ctx			Context pointer.
+ * @param array			Array of cells to read from.
+ * @param numcells		Number of cells to read.
+ */
+int SP_PushCellsFromArray(sp_context_t *ctx, cell_t array[], unsigned int numcells);
+
+/** 
+ * Binds a list of native names and their function pointers to a context.
+ * If num is 0, the list is read until an entry with a NULL name is reached.
+ * All natives are assigned a status of SP_NATIVE_OKAY by default.
+ * If overwrite is non-zero, already registered arrays will be overwritten.
+ * 
+ * @param ctx			Context pointer.
+ * @param natives		Array of natives.
+ * @param num			Number of natives in array.
+ */
+int SP_BindNatives(sp_context_t *ctx, sp_nativeinfo_t *natives, unsigned int num, int overwrite);
+
+/**
+ * Binds a single native.  Overwrites any existing bind.
+ *
+ * @param ctx			Context pointer.
+ * @param native		Pointer to native.
+ * @param status		Status value to set (should be SP_NATIVE_OKAY).
+ */
+int SP_BindNative(sp_context_t *ctx, sp_nativeinfo_t *native, uint32_t status);
+
+/**
+ * Binds a single native to any non-registered native.
+ * Status is automatically set to pending.
+ *
+ * @param ctx			Context pointer.
+ */
+int SP_BindNativeToAny(sp_context_t *ctx, sp_nativeinfo_t *native);
+
+/**
+ * Executes a public function in a context.
+ * The parameter count is set to zero during execution.
+ * All context-specific variables that are modified are saved before execution,
+ *  thus allowing nested calls to SP_Execute().  
+ *
+ * @param ctx			Context pointer.
+ * @param idx			Public function index number.
+ * @param result		Optional pointer to store return value.
+ */
+int SP_Execute(sp_context_t *ctx, uint32_t idx, cell_t *result);
+
+
+#endif //_INCLUDE_SOURCEPAWN_VM_H_
