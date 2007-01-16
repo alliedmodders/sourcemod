e4602813e82a0bf75c48954f635e11c24bd4ff03
David Anderson <dvander@alliedmods.net>
2007-01-16 19:42:05 +0000

    initial import of SDK proper
    --HG--
    extra : convert_revision : svn%3A39bc706e-5318-0410-9160-8a85361fbb7c/trunk%40304

---
+++ extensions/sdk/smsdk_config.h
@@ -0,0 +1,27 @@
+#ifndef _INCLUDE_SOURCEMOD_EXTENSION_CONFIG_H_
+#define _INCLUDE_SOURCEMOD_EXTENSION_CONFIG_H_
+
+/* Basic information exposed publically */
+#define SMEXT_CONF_NAME			"Sample Extension"
+#define SMEXT_CONF_DESCRIPTION	"Sample extension to help developers"
+#define SMEXT_CONF_VERSION		"0.0.0.0"
+#define SMEXT_CONF_AUTHOR		"AlliedModders"
+#define SMEXT_CONF_URL			"http://www.sourcemod.net/"
+#define SMEXT_CONF_LOGTAG		"SAMPLE"
+#define SMEXT_CONF_LICENSE		"GPL"
+#define SMEXT_CONF_DATESTRING	__DATE__
+
+/** 
+ * @brief Exposes plugin's main interface.
+ */
+#define SMEXT_LINK(name) SDKExtension *g_pExtensionIface = name;
+
+/**
+ * @brief Sets whether or not this plugin required Metamod.
+ * NOTE: Uncomment to enable, comment to disable.
+ * NOTE: This is enabled automatically if a Metamod build is chosen in 
+ *		 the Visual Studio project.
+ */
+//#define SMEXT_CONF_METAMOD		
+
+#endif //_INCLUDE_SOURCEMOD_EXTENSION_CONFIG_H_
