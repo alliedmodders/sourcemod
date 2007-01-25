21fb4856e0fad38ba4b7bbb600a291a84c2cebcf
David Anderson <dvander@alliedmods.net>
2007-01-25 22:36:38 +0000

    added license headers
    --HG--
    extra : convert_revision : svn%3A39bc706e-5318-0410-9160-8a85361fbb7c/trunk%40375

--- core/CTranslator.cpp
+++ core/CTranslator.cpp
@@ -1,3 +1,16 @@
+/**
+ * ===============================================================
+ * SourceMod (C)2004-2007 AlliedModders LLC.  All rights reserved.
+ * ===============================================================
+ *
+ * This file is not open source and may not be copied without explicit
+ * written permission of AlliedModders LLC.  This file may not be redistributed 
+ * in whole or significant part.
+ * For information, see LICENSE.txt or http://www.sourcemod.net/license.php
+ *
+ * Version: $Id$
+ */
+
 #include <stdarg.h>
 #include <stdlib.h>
 #include <ctype.h>
