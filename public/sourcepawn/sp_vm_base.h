fcc5f00a2bd7e97b6169aadf010c7c71082445b1
David Anderson <dvander@alliedmods.net>
2007-01-26 04:35:08 +0000

    changed license header once again
    --HG--
    extra : convert_revision : svn%3A39bc706e-5318-0410-9160-8a85361fbb7c/trunk%40384

--- public/sourcepawn/sp_vm_base.h
+++ public/sourcepawn/sp_vm_base.h
@@ -4,11 +4,13 @@
  * All rights reserved.
  * ================================================================
  *
- *  This file is part of the SourceMod/SourcePawn SDK.  This file may only be used 
- * or modified under the Terms and Conditions of its License Agreement, which is found 
- * in LICENSE.txt.  The Terms and Conditions for making SourceMod extensions/plugins 
- * may change at any time.  To view the latest information, see:
- *   http://www.sourcemod.net/license.php
+ *  This file is part of the SourceMod/SourcePawn SDK.  This file may only be 
+ * used or modified under the Terms and Conditions of its License Agreement, 
+ * which is found in public/licenses/LICENSE.txt.  As of this notice, derivative 
+ * works must be licensed under the GNU General Public License (version 2 or 
+ * greater).  A copy of the GPL is included under public/licenses/GPL.txt.
+ * 
+ * To view the latest information, see: http://www.sourcemod.net/license.php
  *
  * Version: $Id$
  */
