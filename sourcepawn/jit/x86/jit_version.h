49c6e38ffa15a06d518277f1d424a1f67c357688
David Anderson <dvander@alliedmods.net>
2007-01-27 03:35:42 +0000

    added version file
    --HG--
    extra : convert_revision : svn%3A39bc706e-5318-0410-9160-8a85361fbb7c/trunk%40396

---
+++ sourcepawn/jit/x86/jit_version.h
@@ -0,0 +1,22 @@
+/**
+ * ================================================================
+ * SourcePawn (C)2004-2007 AlliedModders LLC.  All rights reserved.
+ * ================================================================
+ *
+ * This file is not open source and may not be copied without explicit
+ * written permission of AlliedModders LLC.  This file may not be redistributed 
+ * in whole or significant part.
+ * For information, see LICENSE.txt or http://www.sourcemod.net/license.php
+ *
+ * Version: $Id$
+ */
+
+
+#ifndef _INCLUDE_JIT_VERSION_H_
+#define _INCLUDE_JIT_VERSION_H_
+
+#include "svn_version.h"
+
+#define JIT_VERSION			"1.0.0" SVN_REVISION_STRING
+
+#endif //_INCLUDE_JIT_VERSION_H_
