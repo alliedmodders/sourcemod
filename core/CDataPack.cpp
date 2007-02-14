3287dc14429305813469288b6975733950b2377f
Borja Ferrer <faluco@alliedmods.net>
2007-02-14 11:29:59 +0000

    forgot to add the headers
    --HG--
    extra : convert_revision : svn%3A39bc706e-5318-0410-9160-8a85361fbb7c/trunk%40496

--- core/CDataPack.cpp
+++ core/CDataPack.cpp
@@ -1,3 +1,17 @@
+/**
+* vim: set ts=4 :
+* ===============================================================
+* SourceMod (C)2004-2007 AlliedModders LLC.  All rights reserved.
+* ===============================================================
+*
+* This file is not open source and may not be copied without explicit
+* written permission of AlliedModders LLC.  This file may not be redistributed 
+* in whole or significant part.
+* For information, see LICENSE.txt or http://www.sourcemod.net/license.php
+*
+* Version: $Id$
+*/
+
 #include <malloc.h>
 #include <string.h>
 #include "CDataPack.h"
