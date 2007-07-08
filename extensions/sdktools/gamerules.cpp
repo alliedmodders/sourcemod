0a4f42d7fa02e394f5ee94d2eea331015c39519c
Scott Ehlert <ds@alliedmods.net>
2007-07-08 09:47:43 +0000

    Oops, added some missing files for CGameRules support
    --HG--
    extra : convert_revision : svn%3A39bc706e-5318-0410-9160-8a85361fbb7c/trunk%401072

---
+++ extensions/sdktools/gamerules.cpp
@@ -0,0 +1,53 @@
+/**
+ * vim: set ts=4 :
+ * ===============================================================
+ * SourceMod SDK Tools Extension
+ * Copyright (C) 2004-2007 AlliedModders LLC. All rights reserved.
+ * ===============================================================
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Version: $Id$
+ */
+
+#include "extension.h"
+
+void **g_pGameRules = NULL;
+
+void InitializeGameRules()
+{
+	char *addr = NULL;
+
+#ifdef PLATFORM_WINDOWS
+	int offset = 0;
+
+	if (!g_pGameConf->GetMemSig("CreateGameRulesObject", (void **)&addr) || !addr)
+	{
+		return;
+	}
+	if (!g_pGameConf->GetOffset("g_pGameRules", &offset) || !offset)
+	{
+		return;
+	}
+
+	g_pGameRules = *reinterpret_cast<void ***>(addr + offset);
+#elif defined PLATFORM_LINUX
+	if (!g_pGameRules->GetMemSig("g_pGameRules", (void **)&addr) || !addr)
+	{
+		return;
+	}
+
+	g_pGameRules = reinterpret_cast<void **>(addr);
+#endif
+}
