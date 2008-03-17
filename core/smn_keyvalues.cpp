34204b20438ea277a73a9f887f963491744f5277
David Anderson <dvander@alliedmods.net>
2008-03-17 00:11:01 +0000

    actually fixed build
    --HG--
    extra : convert_revision : svn%3A39bc706e-5318-0410-9160-8a85361fbb7c/trunk%401946

--- core/smn_keyvalues.cpp
+++ core/smn_keyvalues.cpp
@@ -627,33 +627,6 @@ static cell_t smn_KvGotoNextKey(IPluginContext *pCtx, const cell_t *params)
 	return 1;	
 }
 
-static cell_t smn_KvJumpNextSubKey(IPluginContext *pCtx, const cell_t *params)
-{
-	Handle_t hndl = static_cast<Handle_t>(params[1]);
-	HandleError herr;
-	HandleSecurity sec;
-	KeyValueStack *pStk;
-
-	sec.pOwner = NULL;
-	sec.pIdentity = g_pCoreIdent;
-
-	if ((herr=g_HandleSys.ReadHandle(hndl, g_KeyValueType, &sec, (void **)&pStk))
-		!= HandleError_None)
-	{
-		return pCtx->ThrowNativeError("Invalid key value handle %x (error %d)", hndl, herr);
-	}
-
-	KeyValues *pSubKey = pStk->pCurRoot.front();
-	KeyValues *pNextKey = pSubKey->GetNextKey();
-	if (!pNextKey)
-	{
-		return 0;
-	}
-	pStk->pCurRoot.push(pNextKey);
-
-	return 1;	
-}
-
 static cell_t smn_KvGoBack(IPluginContext *pCtx, const cell_t *params)
 {
 	Handle_t hndl = static_cast<Handle_t>(params[1]);
