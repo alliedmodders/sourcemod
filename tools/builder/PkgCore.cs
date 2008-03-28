33b2847ec311222136df9b513ede2d067974033e
Scott Ehlert <ds@alliedmods.net>
2008-03-28 04:51:53 +0000

    Added scripting/adminmenu directory to build tool
    --HG--
    extra : convert_revision : svn%3A39bc706e-5318-0410-9160-8a85361fbb7c/trunk%401968

--- tools/builder/PkgCore.cs
+++ tools/builder/PkgCore.cs
@@ -51,6 +51,7 @@ namespace builder
 			folders.Add("addons/sourcemod/data");
 			folders.Add("addons/sourcemod/scripting/include");
 			folders.Add("addons/sourcemod/scripting/admin-flatfile");
+			folders.Add("addons/sourcemod/scripting/adminmenu");
 			folders.Add("addons/sourcemod/scripting/testsuite");
 			folders.Add("cfg/sourcemod");
 			folders.Add("addons/sourcemod/configs/sql-init-scripts");
@@ -114,6 +115,7 @@ namespace builder
 			builder.CopyFolder(this, "translations", "addons/sourcemod/translations", null);
 			builder.CopyFolder(this, "public/licenses", "addons/sourcemod", null);
 			builder.CopyFolder(this, "plugins/admin-flatfile", "addons/sourcemod/scripting/admin-flatfile", null);
+			builder.CopyFolder(this, "plugins/adminmenu", "addons/sourcemod/scripting/adminmenu", null);
 			builder.CopyFolder(this, "plugins/testsuite", "addons/sourcemod/scripting/testsuite", null);
 			builder.CopyFolder(this, "plugins/basecommands", "addons/sourcemod/scripting/basecommands", null);
 			builder.CopyFolder(this, "plugins/basecomm", "addons/sourcemod/scripting/basecomm", null);
