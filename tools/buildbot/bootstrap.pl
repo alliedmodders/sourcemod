2d78224b917e1b117b565237e8e79c36a75863e4
David Anderson <dvander@alliedmods.net>
2008-09-06 14:41:38 +0000

    Adding buildbot files back.
    --HG--
    branch : sourcemod-1.0.x
    extra : convert_revision : svn%3A39bc706e-5318-0410-9160-8a85361fbb7c/branches/sourcemod-1.0.x%402510

---
+++ tools/buildbot/bootstrap.pl
@@ -0,0 +1,59 @@
+#!/usr/bin/perl
+
+use strict;
+use Cwd;
+use File::Basename;
+
+my ($myself, $path) = fileparse($0);
+chdir($path);
+
+require 'helpers.pm';
+
+chdir(Build::PathFormat('../builder'));
+if ($^O eq "linux")
+{
+    Build::Command('make clean');
+    Build::Command('make');
+}
+else
+{
+    Build::Command('"' . $ENV{'MSVC7'} . '" /Rebuild Release builder.csproj');
+    Build::Command('move ' . Build::PathFormat('bin/Release/builder.exe') . ' .');
+}
+
+die "Unable to build builder tool!\n" unless -e 'builder.exe';
+
+#Go back to main source dir.
+chdir(Build::PathFormat('../..'));
+
+#Get the source path.
+my ($root) = getcwd();
+
+#Create output folder if it doesn't exist.
+if (!(-d 'OUTPUT')) {
+    mkdir('OUTPUT') or die("Failed to create output folder: $!\n");
+}
+
+#Write the configuration file.
+open(CONF, '>build.cfg') or die("Failed to write build.cfg: $!\n");
+print CONF "OutputBase = " . Build::PathFormat($root . '/OUTPUT') . "\n";
+print CONF "SourceBase = $root\n";
+if ($^O eq "linux") 
+{
+    print CONF "BuilderPath = /usr/bin/make\n";
+}
+else
+{
+    print CONF "BuilderPath = " . $ENV{'MSVC8'} . "\n";
+}
+close(CONF);
+
+#Do the annoying revision bumping.
+#Linux needs some help here.
+if ($^O eq "linux")
+{
+    Build::Command("flip -u modules.versions");
+    Build::Command("flip -u tools/versionchanger.pl");
+    Build::Command("chmod +x tools/versionchanger.pl");
+}
+Build::Command(Build::PathFormat('tools/versionchanger.pl'));
