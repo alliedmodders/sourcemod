5caeebf80786fcaeb68d5443856ff3e5f1869ba1
David Anderson <dvander@alliedmods.net>
2008-09-06 23:24:56 +0000

    Bugfixes to buildbot scripts, added symstore script. Removed soon-to-be-outdated fetchdlls program.
    --HG--
    extra : convert_revision : svn%3A39bc706e-5318-0410-9160-8a85361fbb7c/trunk%402512

---
+++ tools/buildbot/symstore.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl
+
+use File::Basename;
+
+my ($myself, $path) = fileparse($0);
+chdir($path);
+
+require 'helpers.pm';
+
+chdir('..');
+chdir('..');
+
+our $SSH = 'ssh -i ../../smpvkey';
+
+open(PDBLOG, 'OUTPUT/pdblog.txt') or die "Could not open pdblog.txt: $!\n";
+
+#Sync us up with the main symbol store
+rsync('sourcemod@alliedmods.net:~/public_html/symbols/', '..\\..\\symstore');
+
+#Get version info
+my ($version);
+$version = Build::ProductVersion(Build::PathFormat('product.version'));
+$version .= '.' . Build::Revision('.');
+
+my ($line);
+while (<PDBLOG>)
+{
+	$line = $_;
+	$line =~ s/\.pdb/\*/;
+	chomp $line;
+	Build::Command("symstore add /r /f \"$line\" /s ..\\..\\symstore /t \"SourceMod\" /v \"$version\" /c \"buildbot\"");
+}
+
+close(PDBLOG);
+
+#Now that we're done, rsync back.
+rsync('..\\..\\symstore\\', 'sourcemod@alliedmods.net:~/public_html/symbols');
+
+sub rsync
+{
+	my ($from, $to) = (@_);
+	
+	Build::Command('rsync -av --delete -e="' . $SSH . '" ' . $from . ' ' . $to);
+}
