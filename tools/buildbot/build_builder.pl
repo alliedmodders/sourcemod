6e930f60485d2894296b5c06903922d728bae8bb
David Anderson <dvander@alliedmods.net>
2008-09-06 07:39:10 +0000

    Added buildbot script for building builder tool.
    --HG--
    extra : convert_revision : svn%3A39bc706e-5318-0410-9160-8a85361fbb7c/trunk%402491

---
+++ tools/buildbot/build_builder.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+
+require 'helpers.pm';
+
+chdir('../builder');
+if ($^O eq "linux")
+{
+    Build::Command('make clean');
+    Build::Command('make');
+}
+else
+{
+    Build::Command($ENV{'MSVC7'} . ' /Rebuild Release builder.csproj');
+    Build::Command('move ' . Build::PathFormat('bin/Release/builder.exe') . ' .');
+}
+
+die "Unable to build builder tool!\n" unless -e 'builder.exe';
+
