097f42a01c6f4353120b28ac1fc3876ff4b754e5
Scott Ehlert <ds@alliedmods.net>
2008-09-14 02:54:50 -0500

    Removed files for dead branch: faluco
    --HG--
    branch : faluco

--- compiler-init/svnrev.h
+++
@@ -1,9 +0,0 @@
-#define SMC_VERSION		1
-#define SMC_REVISION	0
-#define SMC_BUILD		1
-#define SMC_VERSTRING	"1.0.1.3599"
-
-#define SVN_REV		3599
-#define SVN_REVSTR	"3599"
-#define SVN_REVDATE	"2006-07-05"
-#define SVN_REVSTAMP	20060705L
