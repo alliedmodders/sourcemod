bba03b62170ed479666b6e8ebca89173f9af490e
Scott Ehlert <ds@alliedmods.net>
2008-09-14 03:29:04 -0500

    Removed files for dead branch: jit2
    --HG--
    branch : jit2

--- plugins/testsuite/sqltest.sql
+++
@@ -1,7 +0,0 @@
-
-CREATE TABLE gaben (gaben int primary key, fat varchar(32));
-
-INSERT INTO gaben VALUES(1, 'what the');
-INSERT INTO gaben VALUES(2, 'Bee''s Knees!');
-INSERT INTO gaben VALUES(3, 'newell');
-INSERT INTO gaben VALUES(4, 'CRAB CAKE.');
--- sourcepawn/spshell/test.sp
+++
@@ -1,6 +0,0 @@
-#include <sourcemod>
-
-public main2(a[3])
-{
-	return a[0] + a[1] + a[2]
-}
