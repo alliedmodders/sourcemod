5a0528de265078dd2df9a67d54910cc6dcff8dad
jonasfietz <data@geekig.de>
2009-01-13 16:14:44 -0500

    Improve compile.sh to accept filenames (bug 3550, r=dvander).

--- plugins/compile.sh
+++ plugins/compile.sh
@@ -2,10 +2,20 @@
 
 test -e compiled || mkdir compiled
 
+if [[ $# -ne 0 ]]
+then
+    for i in "$@"; 
+    do
+        smxfile="`echo $i | sed -e 's/\.sp$/\.smx/'`";
+	    echo -n "Compiling $i...";
+	    ./spcomp $i -ocompiled/$smxfile
+    done
+else
+
 for sourcefile in *.sp
 do
 	smxfile="`echo $sourcefile | sed -e 's/\.sp$/\.smx/'`"
 	echo -n "Compiling $sourcefile ..."
 	./spcomp $sourcefile -ocompiled/$smxfile
 done
-
+fi
