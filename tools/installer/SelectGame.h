3f264097a76ea17d992113a0e86915cbcd76f550
David Anderson <dvander@alliedmods.net>
2009-10-02 03:30:54 -0700

    Removed unused files.

--- tools/installer/SelectGame.h
+++
@@ -1,8 +0,0 @@
-#ifndef _INCLUDE_INSTALLER_SELECT_GAME_H_
-#define _INCLUDE_INSTALLER_SELECT_GAME_H_
-
-#include "InstallerMain.h"
-
-void *DisplaySelectGame(HWND hWnd);
-
-#endif //_INCLUDE_INSTALLER_SELECT_GAME_H_
