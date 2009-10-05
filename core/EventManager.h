f2b201aed84b0d600c2760869f627a647eb74396
Scott Ehlert <ds@alliedmods.net>
2009-10-05 03:37:09 -0500

    You're not seeing this right now. Temporary build fix maybe.

--- core/EventManager.h
+++ core/EventManager.h
@@ -25,8 +25,6 @@
  * this exception to all derivative works.  AlliedModders LLC defines further
  * exceptions, found in LICENSE.txt (as of this writing, version JULY-31-2007),
  * or <http://www.sourcemod.net/license.php>.
- *
- * Version: $Id$
  */
 
 #ifndef _INCLUDE_SOURCEMOD_EVENTMANAGER_H_
