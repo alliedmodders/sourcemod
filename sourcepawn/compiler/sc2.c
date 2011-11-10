0c926b84bc711aeccab90eefed2853bf8ad8dc95
Nicholas Hastings <skamonkey@gmail.com>
2011-11-09 21:29:36 -0500

    Another build fix.

--- sourcepawn/compiler/sc2.c
+++ sourcepawn/compiler/sc2.c
@@ -59,14 +59,6 @@ static short skiplevel; /* level at which we started skipping (including nested 
 static unsigned char term_expr[] = "";
 static int listline=-1; /* "current line" for the list file */
 
-#if defined __GNUC__
-static double pow10(double d)
-{
-  return pow(10, d);
-}
-#endif
-
-
 /*  pushstk & popstk
  *
  *  Uses a LIFO stack to store information. The stack is used by doinclude(),
