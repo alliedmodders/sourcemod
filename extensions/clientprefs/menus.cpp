618eb254c046779b2a28e43427de7bda426e7823
Asher Baker <asherkin@gmail.com>
2012-05-29 17:41:48 +0100

    Fixed intermittent crashes in clientprefs (bug 4660, r=psychonic).

--- extensions/clientprefs/menus.cpp
+++ extensions/clientprefs/menus.cpp
@@ -40,7 +40,7 @@ void ClientMenuHandler::OnMenuSelect(IBaseMenu *menu, int client, unsigned int i
 
 	const char *info = menu->GetItemInfo(item, &draw);
 
-	AutoMenuData *data = (AutoMenuData *)strtol(info, NULL, 16);
+	AutoMenuData *data = (AutoMenuData *)strtoul(info, NULL, 16);
 
 	if (data->handler->forward != NULL)
 	{
@@ -94,7 +94,7 @@ unsigned int ClientMenuHandler::OnMenuDisplayItem(IBaseMenu *menu,
 
 	const char *info = menu->GetItemInfo(item, &draw);
 
-	AutoMenuData *data = (AutoMenuData *)strtol(info, NULL, 16);
+	AutoMenuData *data = (AutoMenuData *)strtoul(info, NULL, 16);
 
 	if (data->handler->forward != NULL)
 	{
@@ -122,7 +122,7 @@ void AutoMenuHandler::OnMenuSelect(SourceMod::IBaseMenu *menu, int client, unsig
 
 	const char *info = menu->GetItemInfo(item, &draw);
 
-	AutoMenuData *data = (AutoMenuData *)strtol(info, NULL, 16);
+	AutoMenuData *data = (AutoMenuData *)strtoul(info, NULL, 16);
 
 	switch (data->type)
 	{
