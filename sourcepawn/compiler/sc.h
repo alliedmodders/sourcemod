f1e01a3cf6fc0d46d6bebeeab329e4ea8e2b0787
Zach Kanzler <theY4Kman@gmail.com>
2012-08-04 13:08:08 -0700

    Increase maximum .sp line length to 4095 characters. (bug 5347, r=dvander)

--- sourcepawn/compiler/sc.h
+++ sourcepawn/compiler/sc.h
@@ -51,7 +51,7 @@
 #define sCHARBITS   8       /* size of a packed character */
 
 #define sDIMEN_MAX     4    /* maximum number of array dimensions */
-#define sLINEMAX     1023   /* input line length (in characters) */
+#define sLINEMAX     4095   /* input line length (in characters) */
 #define sCOMP_STACK   32    /* maximum nesting of #if .. #endif sections */
 #define sDEF_LITMAX  500    /* initial size of the literal pool, in "cells" */
 #define sDEF_AMXSTACK 4096  /* default stack size for AMX files */
