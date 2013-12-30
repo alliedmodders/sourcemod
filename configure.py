e1a820dcf90a8609da9eea83411582aebd3f20aa
David Anderson <dvander@alliedmods.net>
2013-12-30 17:50:56 -0500

    Port simple C++ build scripts to AMBuild 2 (bug 5997 part 1, r=ds).

--- configure.py
+++ configure.py
@@ -1,8 +1,19 @@
 # vim: set ts=2 sw=2 tw=99 noet:
 import sys
-import ambuild.runner as runner
+try:
+	from ambuild2 import run
+except:
+	try:
+		import ambuild
+		sys.stderr.write('It looks like you have AMBuild 1 installed, but this project uses AMBuild 2.\n')
+		sys.stderr.write('Upgrade to the latest version of AMBuild to continue.\n')
+	except:
+		sys.stderr.write('AMBuild must be installed to build this project.\n')
+		sys.stderr.write('http://www.alliedmods.net/ambuild\n')
+	sys.exit(1)
 
-run = runner.Runner()
+run = run.PrepareBuild(sourcePath=sys.path[0])
+run.default_build_folder = 'obj-' + run.target_platform
 run.options.add_option('--enable-debug', action='store_const', const='1', dest='debug',
                        help='Enable debugging symbols')
 run.options.add_option('--enable-optimize', action='store_const', const='1', dest='opt',
@@ -12,4 +23,4 @@ run.options.add_option('--no-mysql', action='store_false', default=True, dest='h
 run.options.add_option('-s', '--sdks', default='all', dest='sdks',
                        help='Build against specified SDKs; valid args are "all", "present", or '
                             'comma-delimited list of engine names (default: %default)')
-run.Configure(sys.path[0])
+run.Configure()
