c47a5b7a14e92f526324cb98fccac84c1ffaece5
David Anderson <dvander@alliedmods.net>
2014-05-26 22:12:00 -0700

    Fix build.

--- tools/buildbot/bootstrap.pl
+++ tools/buildbot/bootstrap.pl
@@ -37,13 +37,13 @@ my @conf_argv = (
 );
 
 if ($^O =~ /darwin/) {
-	push(@conf_argv, '--hl2sdk-root=/hgshare');
+	push(@conf_argv, '--hl2sdk-root=/Volumes/hgshare');
 	push(@conf_argv, '--mms-path=/Users/builds/slaves/common/mmsource-1.10');
 	push(@conf_argv, '--mysql-path=/Users/builds/slaves/common/mysql-5.0');
 } elsif ($^O =~ /linux/) {
-	push(@conf_argv, '--hl2sdk-root=/Volumes/hgshare');
-	push(@conf_argv, '--mms-path=/Users/builds/common/mmsource-1.10');
-	push(@conf_argv, '--mysql-path=/Users/builds/common/mysql-5.0');
+  push(@conf_argv, '--hl2sdk-root=/hgshare');
+  push(@conf_argv, '--mms-path=/home/builds/common/mmsource-1.10');
+  push(@conf_argv, '--mysql-path=/home/builds/common/mysql-5.0');
 } elsif ($^O =~ /MSWin/) {
 	push(@conf_argv, '--hl2sdk-root=H:\\');
 }
