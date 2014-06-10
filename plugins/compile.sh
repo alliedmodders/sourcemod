96779155794c8a442508b5fbfd109524636554cf
WildCard65 <lollol222gg@gmail.com>
2014-06-10 06:44:11 -0400

    Replaced all spaces/tabs with just tabs (if they end up becoming spaces, blame gedit)

--- plugins/compile.sh
+++ plugins/compile.sh
@@ -1,30 +1,28 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-
 test -e compiled || mkdir compiled
 
 if [[ $# -ne 0 ]]
 then
-    for i in "$@"; 
-    do
-        smxfile="`echo $i | sed -e 's/\.sp$/\.smx/'`";
-	    echo -e "\nCompiling $i...";
-	    ./spcomp $i -ocompiled/$smxfile
-	    RETVAL=$?
-	    if [ $RETVAL -ne 0 ]; then
-	    	exit 1;
-	    fi
-    done
+	for i in "$@"; 
+	do
+		smxfile="`echo $i | sed -e 's/\.sp$/\.smx/'`";
+		echo -e "\nCompiling $i...";
+		./spcomp $i -ocompiled/$smxfile
+		RETVAL=$?
+		if [ $RETVAL -ne 0 ]; then
+			exit 1;
+		fi
+	done
 else
-
-for sourcefile in *.sp
-do
-	smxfile="`echo $sourcefile | sed -e 's/\.sp$/\.smx/'`"
-	echo -e "\nCompiling $sourcefile ..."
-	./spcomp $sourcefile -ocompiled/$smxfile
-	RETVAL=$?
-	if [ $RETVAL -ne 0 ]; then
-	    exit 1;
-	fi
-done
+	for sourcefile in *.sp
+	do
+		smxfile="`echo $sourcefile | sed -e 's/\.sp$/\.smx/'`"
+		echo -e "\nCompiling $sourcefile ..."
+		./spcomp $sourcefile -ocompiled/$smxfile
+		RETVAL=$?
+		if [ $RETVAL -ne 0 ]; then
+			exit 1;
+		fi
+	done
 fi
