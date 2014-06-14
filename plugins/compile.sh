8e4836f83a4adc94d482393c00b935f2860d275a
WildCard65 <lollol222gg@gmail.com>
2014-06-14 08:25:19 -0400

    Added spacing to compile.sh And made it exit out on compile error.

--- plugins/compile.sh
+++ plugins/compile.sh
@@ -3,20 +3,26 @@ cd "$(dirname "$0")"
 
 test -e compiled || mkdir compiled
 
-if [[ $# -ne 0 ]]
-then
-    for i in "$@"; 
-    do
-        smxfile="`echo $i | sed -e 's/\.sp$/\.smx/'`";
-	    echo -n "Compiling $i...";
-	    ./spcomp $i -ocompiled/$smxfile
-    done
+if [[ $# -ne 0 ]]; then
+	for i in "$@"; 
+	do
+		smxfile="`echo $i | sed -e 's/\.sp$/\.smx/'`";
+		echo -e "Compiling $i...";
+		./spcomp $i -ocompiled/$smxfile
+		RETVAL=$?
+		if [ $RETVAL -ne 0 ]; then
+			exit 1;
+		fi
+	done
 else
-
-for sourcefile in *.sp
-do
-	smxfile="`echo $sourcefile | sed -e 's/\.sp$/\.smx/'`"
-	echo -n "Compiling $sourcefile ..."
-	./spcomp $sourcefile -ocompiled/$smxfile
-done
+	for sourcefile in *.sp
+	do
+		smxfile="`echo $sourcefile | sed -e 's/\.sp$/\.smx/'`"
+		echo -e "Compiling $sourcefile ..."
+		./spcomp $sourcefile -ocompiled/$smxfile
+		RETVAL=$?
+		if [ $RETVAL -ne 0 ]; then
+			exit 1;
+		fi
+	done
 fi
