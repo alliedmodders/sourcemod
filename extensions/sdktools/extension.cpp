alliedmodders/sourcemod

commit 90b3df1181ed1684cd7daaca55c267fb46a7da33

Add 2 CSGO gamerules netprops to CSGO blocklist if FollowCSGOServerGuidelines is enabled. (#514)
* Add m_bIsValveDS and m_bIsQuestEligible

* Fix compile

extensions/sdktools/extension.cpp
@@ -177,6 +177,9 @@ bool SDKTools::SDK_OnLoad(char *error, size_t maxlength, bool late)
 	{
 		m_bFollowCSGOServerGuidelines = false;
 	}
+
+	m_CSGOBadList.add("m_bIsValveDS");
+	m_CSGOBadList.add("m_bIsQuestEligible");
 #endif
 
 	return true;

extensions/sdktools/extension.h
@@ -50,6 +50,10 @@
 #include <convar.h>
 #include <iserver.h>
 #include <cdll_int.h>
+#if SOURCE_ENGINE == SE_CSGO
+#include <am-hashset.h>
+#include <sm_stringhashmap.h>
+#endif
 #include "SoundEmitterSystem/isoundemittersystembase.h"
 
 #if SOURCE_ENGINE >= SE_ORANGEBOX
@@ -111,10 +115,15 @@ public:
 	void OnServerActivate(edict_t *pEdictList, int edictCount, int clientMax);
 public:
 	bool HasAnyLevelInited() { return m_bAnyLevelInited; }
+#if SOURCE_ENGINE == SE_CSGO
+public:
 	bool ShouldFollowCSGOServerGuidelines() const { return m_bFollowCSGOServerGuidelines; }
-
+	bool CanSetCSGOEntProp(const char *pszPropName) { return !ShouldFollowCSGOServerGuidelines() || !m_CSGOBadList.has(pszPropName); }
 private:
+	ke::HashSet<ke::AString, detail::StringHashMapPolicy> m_CSGOBadList;
 	bool m_bFollowCSGOServerGuidelines = false;
+#endif
+private:
 	bool m_bAnyLevelInited = false;
 };
 

extensions/sdktools/gamerulesnatives.cpp
@@ -183,7 +183,8 @@ static cell_t GameRules_GetProp(IPluginContext *pContext, const cell_t *params)
 		bit_count = sizeof(int) * 8;
 	}
 #endif
-	if (bit_count < 1)
+
+	if (bit_count < 1)
 	{
 		bit_count = params[2] * 8;
 	}
@@ -239,6 +240,13 @@ static cell_t GameRules_SetProp(IPluginContext *pContext, const cell_t *params)
 		return pContext->ThrowNativeError("Gamerules lookup failed");
 
 	pContext->LocalToString(params[1], &prop);
+
+#if SOURCE_ENGINE == SE_CSGO
+	if (!g_SdkTools.CanSetCSGOEntProp(prop))
+	{
+		return pContext->ThrowNativeError("Cannot set ent prop %s with core.cfg option \"FollowCSGOServerGuidelines\" enabled.", prop);
+	}
+#endif
 
 	FIND_PROP_SEND(DPT_Int, "integer");
 
@@ -318,6 +326,13 @@ static cell_t GameRules_SetPropFloat(IPluginContext *pContext, const cell_t *par
 
 	pContext->LocalToString(params[1], &prop);
 
+#if SOURCE_ENGINE == SE_CSGO
+	if (!g_SdkTools.CanSetCSGOEntProp(prop))
+	{
+		return pContext->ThrowNativeError("Cannot set ent prop %s with core.cfg option \"FollowCSGOServerGuidelines\" enabled.", prop);
+	}
+#endif
+
 	FIND_PROP_SEND(DPT_Float, "float");
 
 	float newVal = sp_ctof(params[2]);
@@ -375,6 +390,13 @@ static cell_t GameRules_SetPropEnt(IPluginContext *pContext, const cell_t *param
 		return pContext->ThrowNativeError("Gamerules lookup failed.");
 
 	pContext->LocalToString(params[1], &prop);
+
+#if SOURCE_ENGINE == SE_CSGO
+	if (!g_SdkTools.CanSetCSGOEntProp(prop))
+	{
+		return pContext->ThrowNativeError("Cannot set ent prop %s with core.cfg option \"FollowCSGOServerGuidelines\" enabled.", prop);
+	}
+#endif
 
 	FIND_PROP_SEND(DPT_Int, "integer");
 
@@ -450,6 +472,13 @@ static cell_t GameRules_SetPropVector(IPluginContext *pContext, const cell_t *pa
 		return pContext->ThrowNativeError("Gamerules lookup failed.");
 
 	pContext->LocalToString(params[1], &prop);
+
+#if SOURCE_ENGINE == SE_CSGO
+	if (!g_SdkTools.CanSetCSGOEntProp(prop))
+	{
+		return pContext->ThrowNativeError("Cannot set ent prop %s with core.cfg option \"FollowCSGOServerGuidelines\" enabled.", prop);
+	}
+#endif
 
 	FIND_PROP_SEND(DPT_Vector, "vector");
 

commit 04055c411eeff272f7471f4b0c7bc90dfed063f6

Fix startup crash on CS:GO caused by PR #514

extensions/sdktools/extension.cpp
@@ -178,6 +178,7 @@ bool SDKTools::SDK_OnLoad(char *error, size_t maxlength, bool late)
 		m_bFollowCSGOServerGuidelines = false;
 	}
 
+	m_CSGOBadList.init();
 	m_CSGOBadList.add("m_bIsValveDS");
 	m_CSGOBadList.add("m_bIsQuestEligible");
 #endif
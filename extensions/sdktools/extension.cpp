04055c411eeff272f7471f4b0c7bc90dfed063f6
Nicholas Hastings <nshastings@gmail.com>
2017-02-06 08:25:16 -0500

    Fix startup crash on CS:GO caused by PR #514

--- extensions/sdktools/extension.cpp
+++ extensions/sdktools/extension.cpp
@@ -178,6 +178,7 @@ bool SDKTools::SDK_OnLoad(char *error, size_t maxlength, bool late)
 		m_bFollowCSGOServerGuidelines = false;
 	}
 
+	m_CSGOBadList.init();
 	m_CSGOBadList.add("m_bIsValveDS");
 	m_CSGOBadList.add("m_bIsQuestEligible");
 #endif
