4174a0e44198e2e3ffc89567126fe759a0047604
Asher Baker <asherkin@limetech.io>
2018-07-31 11:32:33 +0100

    Restore CDataPack documentation

--- core/logic/CDataPack.h
+++ core/logic/CDataPack.h
@@ -56,22 +56,118 @@ public:
     static void Free(CDataPack *pack);
 
 public: // Originally IDataReader
+	/**
+	 * @brief Resets the position in the data stream to the beginning.
+	 */
 	void Reset() const;
+
+	/**
+	 * @brief Retrieves the current stream position.
+	 *
+	 * @return			Index into the stream.
+	 */
 	size_t GetPosition() const;
+
+	/**
+	 * @brief Sets the current stream position.
+	 *
+	 * @param pos		Index to set the stream at.
+	 * @return			True if succeeded, false if out of bounds.
+	 */
 	bool SetPosition(size_t pos) const;
+
+	/**
+	 * @brief Reads one cell from the data stream.
+	 *
+	 * @return			A cell read from the current position.
+	 */
 	cell_t ReadCell() const;
+
+	/**
+	 * @brief Reads one float from the data stream.
+	 *
+	 * @return			A float read from the current position.
+	 */
 	float ReadFloat() const;
+
+	/**
+	 * @brief Returns whether or not a specified number of bytes from the current stream
+	 *  position to the end can be read.
+	 *
+	 * @param bytes		Number of bytes to simulate reading.
+	 * @return			True if can be read, false otherwise.
+	 */
 	bool IsReadable(size_t bytes = 0) const;
+
+	/**
+	 * @brief Reads a string from the data stream.
+	 *
+	 * @param len		Optional pointer to store the string length.
+	 * @return			Pointer to the string, or NULL if out of bounds.
+	 */
 	const char *ReadString(size_t *len) const;
+
+	/**
+	 * @brief Reads the current position as a generic data type.
+	 *
+	 * @param size		Optional pointer to store the size of the data type.
+	 * @return			Pointer to the data, or NULL if out of bounds.
+	 */
 	void *ReadMemory(size_t *size) const;
+
+	/**
+	 * @brief Reads a function pointer from the data stream.
+	 *
+	 * @return			A function pointer read from the current position.
+	 */
 	cell_t ReadFunction() const;
 
 public: // Originally IDataPack
+	/**
+	 * @brief Resets the used size of the stream back to zero.
+	 */
 	void ResetSize();
+
+	/**
+	 * @brief Packs one cell into the data stream.
+	 *
+	 * @param cell		Cell value to write.
+	 */
 	void PackCell(cell_t cell);
+
+	/**
+	 * @brief Packs one float into the data stream.
+	 *
+	 * @param val		Float value to write.
+	 */
 	void PackFloat(float val);
+
+	/**
+	 * @brief Packs one string into the data stream.
+	 * The length is recorded as well for buffer overrun protection.
+	 *
+	 * @param string	String to write.
+	 */
 	void PackString(const char *string);
+
+	/**
+	 * @brief Creates a generic block of memory in the stream.
+	 *
+	 * Note that the pointer it returns can be invalidated on further
+	 * writing, since the stream size may grow.  You may need to double back
+	 * and fetch the pointer again.
+	 *
+	 * @param size		Size of the memory to create in the stream.
+	 * @param addr		Optional pointer to store the relocated memory address.
+	 * @return			Current position of the stream beforehand.
+	 */
 	size_t CreateMemory(size_t size, void **addr);
+
+	/**
+	 * @brief Packs one function pointer into the data stream.
+	 *
+	 * @param function	The function pointer to write.
+	 */
 	void PackFunction(cell_t function);
 
 public:
