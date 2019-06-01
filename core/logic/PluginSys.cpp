3f19c39d13f3d80750abb7a3bffddff2a174737d
Marcos Teixeira <martlegion@hotmail.com>
2019-06-01 15:50:35 -0300

    Update PluginSys.cpp

--- core/logic/PluginSys.cpp
+++ core/logic/PluginSys.cpp
@@ -1702,7 +1702,7 @@ void CPluginManager::OnRootConsoleCommand(const char *cmdname, const ICommandArg
 			unsigned int id = 1;
 			int plnum = GetPluginCount();
 			char plstr[10];
-			ke::SafeSprintf(plstr, sizeof(plstr), "%i", plnum);
+			ke::SafeSprintf(plstr, sizeof(plstr), "%d", plnum);
 			int plpadding = strlen(plstr);
 
 			if (!plnum)
