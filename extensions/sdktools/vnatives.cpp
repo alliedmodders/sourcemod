1f2cdfd3d717a2bc77483ff3532ea5cf5cf552d6
A1mDev <mitirew@mail.ru>
2021-09-29 20:29:10 +0700

    Add x64 support
    Add x64 support

--- extensions/sdktools/vnatives.cpp
+++ extensions/sdktools/vnatives.cpp
@@ -255,9 +255,8 @@ static cell_t GiveNamedItem(IPluginContext *pContext, const cell_t *params)
 	DECODE_VALVE_PARAM(1, thisinfo, 0);
 	DECODE_VALVE_PARAM(2, vparams, 0);
 	DECODE_VALVE_PARAM(3, vparams, 1);
-	*(bool *)(vptr + 12) = false;
-	*(void **)(vptr + 16) = NULL;
-	
+	*(bool *)(vptr + sizeof(void *) + sizeof(void *) + sizeof(int)) = false;
+	*(void **)(vptr + sizeof(void *) + sizeof(void *) + sizeof(int) + sizeof(int)) = NULL;
 	FINISH_CALL_SIMPLE(&pEntity);
 	
 	return gamehelpers->EntityToBCompatRef(pEntity);
