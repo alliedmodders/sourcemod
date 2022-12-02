93df97dfb497815502360b4c7498d8f55cfaa14c
Peace-Maker <peace-maker@wcfan.de>
2022-12-02 19:36:22 +0100

    Fix SDKHook_GetMaxHealth callback result handling
    The returned result of the last callback in the list was used instead
    of the highest value. This differs from the behavior of the other hooks.
    The returned health is only changed if no other plugin wants to block the callback.

--- extensions/sdkhooks/extension.cpp
+++ extensions/sdkhooks/extension.cpp
@@ -1046,7 +1046,7 @@ int SDKHooks::Hook_GetMaxHealth()
 
 		int new_max = original_max;
 
-		cell_t res = Pl_Continue;
+		cell_t ret = Pl_Continue;
 
 		std::vector<IPluginFunction *> callbackList;
 		PopulateCallbackList(vtablehooklist[entry]->hooks, callbackList, entity);
@@ -1055,10 +1055,20 @@ int SDKHooks::Hook_GetMaxHealth()
 			IPluginFunction *callback = callbackList[entry];
 			callback->PushCell(entity);
 			callback->PushCellByRef(&new_max);
+
+			cell_t res;
 			callback->Execute(&res);
+
+			if (res > ret)
+			{
+				ret = res;
+			}
 		}
 
-		if (res >= Pl_Changed)
+		if (ret >= Pl_Handled)
+			RETURN_META_VALUE(MRES_SUPERCEDE, original_max);
+
+		if (ret >= Pl_Changed)
 			RETURN_META_VALUE(MRES_SUPERCEDE, new_max);
 
 		break;
