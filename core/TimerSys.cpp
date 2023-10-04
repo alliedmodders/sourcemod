5aec5831d306c6c311f532c7d3b09c93c646f42f
stephanie sappho lenzo <sappho@sappho.io>
2023-10-04 01:20:57 -0400

    undo leftover timersys changes

--- core/TimerSys.cpp
+++ core/TimerSys.cpp
@@ -36,6 +36,8 @@
 #include "ConVarManager.h"
 #include "logic_bridge.h"
 
+#define TIMER_MIN_ACCURACY		0.1
+
 TimerSystem g_Timers;
 double g_fUniversalTime = 0.0f;
 float g_fGameStartTime = 0.0f;	/* Game game start time, non-universal */
@@ -144,7 +146,7 @@ private:
  */
 inline double CalcNextThink(double last, float interval)
 {
-	if (g_fUniversalTime - last - interval <= gpGlobals->interval_per_tick)
+	if (g_fUniversalTime - last - interval <= TIMER_MIN_ACCURACY)
 	{
 		return last + interval;
 	}
@@ -236,7 +238,7 @@ void TimerSystem::GameFrame(bool simulating)
 	{
 		RunFrame();
 
-		g_fTimerThink = CalcNextThink(g_fTimerThink, gpGlobals->interval_per_tick);
+		g_fTimerThink = CalcNextThink(g_fTimerThink, TIMER_MIN_ACCURACY);
 	}
 
 	RunFrameHooks(simulating);
