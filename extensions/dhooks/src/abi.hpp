841464434f016766700e4663eaaa7fcc40be27f7
Kenzzer <14257866+kenzzer@users.noreply.github.com>
2026-03-01 20:14:52 +0000

    Fix this ptr crash in Dynamic Detour &  make call original

--- extensions/dhooks/src/abi.hpp
+++ extensions/dhooks/src/abi.hpp
@@ -18,7 +18,7 @@ bool AreParamsEquivalent(const std::vector<Variable>& params_lh, const std::vect
 bool Proccess(sp::CallingConvention conv, std::vector<Variable>& params, ReturnVariable& ret, size_t& stack_size);
 void JIT_CallMemberFunction(AsmJit& jit, bool save_general_register[MAX_GENERAL_REGISTERS], bool save_float_register[MAX_FLOAT_REGISTERS], void* this_ptr, const void* mfp, bool post);
 void JIT_MakeReturn(AsmJit& jit, ReturnVariable& ret);
-void JIT_CallOriginal(AsmJit& jit, ReturnVariable& ret, std::uintptr_t* original_function, std::uintptr_t* call_function);
+void JIT_CallOriginal(AsmJit& jit, ReturnVariable& ret, std::uintptr_t* original_function, size_t stack_size, std::uintptr_t* jit_start);
 void JIT_Recall(AsmJit& jit, bool save_general_register[MAX_GENERAL_REGISTERS], bool save_float_register[MAX_FLOAT_REGISTERS], size_t stack_size, std::uintptr_t* call_function);
 
 }
