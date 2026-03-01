c4313c700791dad6b5a2d8ea7e09e49ed682ffbb
Kenzzer <14257866+kenzzer@users.noreply.github.com>
2026-03-01 20:13:51 +0000

    Enable dhooks natives

--- extensions/dhooks/src/main.cpp
+++ extensions/dhooks/src/main.cpp
@@ -1,12 +1,25 @@
 #include "globals.hpp"
+#include "natives.hpp"
+#include "signatures.hpp"
+#include "handle.hpp"
 
 #include <smsdk_ext.h>
+
+std::vector<sp_nativeinfo_t> gNatives;
 
 class ExtensionBridge : public SDKExtension {
 	virtual bool SDK_OnLoad(char *error, size_t maxlength, bool late) override {
 		if (dhooks::globals::init(g_SMAPI, myself, g_pShareSys, error, maxlength, late) == false) {
 			return false;
 		}
+		dhooks::handle::init();
+
+		gNatives.reserve(1000);
+		dhooks::natives::init(gNatives);
+		gNatives.push_back({nullptr, nullptr});
+		sharesys->AddNatives(myself, gNatives.data());
+
+		dhooks::globals::gameconfs->AddUserConfigHook("Functions", &dhooks::globals::dhooks_config);
 		return true;
 	}
 };
---
+++ extensions/dhooks/src/natives.cpp
@@ -0,0 +1,14 @@
+#include "natives.hpp"
+#include "natives/dhookparam.hpp"
+#include "natives/dhookreturn.hpp"
+#include "natives/dhooksetup.hpp"
+#include "natives/dynamicdetour.hpp"
+
+namespace dhooks::natives {
+void init(std::vector<sp_nativeinfo_t>& natives) {
+	dhookparam::init(natives);
+	dhookreturn::init(natives);
+	dhooksetup::init(natives);
+	dynamicdetour::init(natives);
+}
+}
