7dd8ca869c313a7490847f561c6718ba33568841
Kenzzer <14257866+kenzzer@users.noreply.github.com>
2026-03-07 14:24:16 +0000

    Fix var name error in PlayerManager.cpp

--- core/PlayerManager.cpp
+++ core/PlayerManager.cpp
@@ -113,7 +113,7 @@ PlayerManager::PlayerManager() :
 #endif
 	m_HookServerActivate(&IServerGameDLL::ServerActivate, this, nullptr, &PlayerManager::OnServerActivate),
 #if SOURCE_ENGINE >= SE_LEFT4DEAD
-	m_HookServerHibernationUpdate(&IServerGameDLL::ServerHibernationUpdate, this, nullptr, &PlayerManager::OnServerHibernationUpdate),
+	m_HookOnServerHibernationUpdate(&IServerGameDLL::ServerHibernationUpdate, this, nullptr, &PlayerManager::OnServerHibernationUpdate),
 #elif SOURCE_ENGINE > SE_EYE
 	m_HookSetServerHibernation(&IServerGameDLL::SetServerHibernation, this, nullptr, &PlayerManager::OnServerHibernationUpdate),
 #endif
@@ -169,7 +169,7 @@ void PlayerManager::OnSourceModAllInitialized()
 	m_HookClientSettingsChanged.Add(serverClients);
 	m_HookServerActivate.Add(gamedll);
 #if SOURCE_ENGINE >= SE_LEFT4DEAD
-	m_HookServerHibernationUpdate.Add(gamedll);
+	m_HookOnServerHibernationUpdate.Add(gamedll);
 #elif SOURCE_ENGINE > SE_EYE // 2013/orangebox, but not original orangebox.
 	m_HookSetServerHibernation.Add(gamedll);
 #endif
@@ -226,7 +226,7 @@ void PlayerManager::OnSourceModShutdown()
 	m_HookClientSettingsChanged.Remove(serverClients);
 	m_HookServerActivate.Remove(gamedll);
 #if SOURCE_ENGINE >= SE_LEFT4DEAD
-	m_HookServerHibernationUpdate.Remove(gamedll);
+	m_HookOnServerHibernationUpdate.Remove(gamedll);
 #elif SOURCE_ENGINE > SE_EYE // 2013/orangebox, but not original orangebox.
 	m_HookSetServerHibernation.Remove(gamedll);
 #endif
